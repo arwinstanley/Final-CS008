c302061049dac4de03aa04d3f93df6caaa3db12a
Shauna Kimura <44296621+shaunakimura@users.noreply.github.com>
2018-11-28 13:02:23 -0500

    Add files via upload
    Basic template for gallery

--- gallery.php
+++ gallery.php
@@ -2,7 +2,32 @@
 include ('top.php');
 ?>
 <main>
-    <h2>Gallery<h2>
+    
+    <!-- Basic Layout of Gallery -->
+    
+    <h2>Gallery</h2>
+    
+    <figure class="galleryImgA">
+        <img alt="" src="<!-- Insert Image -->">
+        <article> <!-- Insert Text --> </article>
+    </figure>
+    
+     <figure class="galleryImgB">
+        <img alt="" src="<!-- Insert Image -->">
+        <article> <!-- Insert Text --> </article>
+    </figure>
+    
+    <figure class="galleryImgC">
+        <img alt="" src="<!-- Insert Image -->">
+        <article> <!-- Insert Text --> </article>
+    </figure>
+    
+    <figure class="galleryImgD">
+        <img alt="" src="<!-- Insert Image -->">
+        <article> <!-- Insert Text --> </article>
+    </figure>
+    
+    
 </main>
 
 <?php
