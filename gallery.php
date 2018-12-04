159bde6509e8d0cedf45aee7470cd6d74145ca16
Shauna Kimura <44296621+shaunakimura@users.noreply.github.com>
2018-12-03 23:59:03 -0500

    Add files via upload
    gallery, news

--- gallery.php
+++ gallery.php
@@ -1,38 +1,72 @@
 <?php
-include ('top.php');
+/*
+ * this demo modifies the code from Uvm Web tools: 
+ * http://www.uvm.edu/webguide/tools/?Page=photogallery.html
+ * 
+ * for this responsive slider the images must be large enough to fit on a full 
+ * size screen mine are 1200px wide. smaller images get resized to fit the full
+ * window. so a small 400px wide image woudl be stretched to 1200px wide or 
+ * whatever the size of the monitor is.
+ * 
+ * 
+ * Added to this sample is using php to get a list of images in a folder so all
+ * you do to update the slide is change the images in the folder. to change the
+ * order just change the file names as they will be abc order. Notice the source
+ * code has the images in a different order than the first example
+ */
+
+// this function gets all the jpg and png images locaged in the $url
+// It returns an array with the image names. folder must not have an index file
+function getFileList($url, $extensions = array("jpg", "png")) {
+    $outputBuffer = array();
+
+    $dir = scandir($url);
+
+    if (count($dir) > 0) {
+        //Start at index 2, to ignore the ".." and "." folders
+        for ($i = 2; $i < count($dir); $i++) {
+            //Only add files to the image array that have the expected extension
+            $ext = pathinfo($dir[$i], PATHINFO_EXTENSION);
+            if (in_array($ext, $extensions)) {
+                array_push($outputBuffer, $dir[$i]);
+            }
+        }
+    }
+
+    return $outputBuffer;
+}
+
+// folder that has the images. you can add a path if needed
+$url = "images";
+
+//This array has all our images in it
+$images = getFileList($url);
+
+include('top.php');
 ?>
-<main>
-    
-    <!-- Basic Layout of Gallery -->
-    
+
+
+<main id="main">
+
+   
+    <button class="openbtn" onclick="openNav()">&#9776; Menu</button> 
     <h2>Gallery</h2>
-    
-    <figure class="galleryImgA">
-        <img alt="" src="<!-- Insert Image -->">
-        <article> <!-- Insert Text --> </article>
-    </figure>
-    
-     <figure class="galleryImgB">
-        <img alt="" src="<!-- Insert Image -->">
-        <article> <!-- Insert Text --> </article>
-    </figure>
-    
-    <figure class="galleryImgC">
-        <img alt="" src="<!-- Insert Image -->">
-        <article> <!-- Insert Text --> </article>
-    </figure>
-    
-    <figure class="galleryImgD">
-        <img alt="" src="<!-- Insert Image -->">
-        <article> <!-- Insert Text --> </article>
-    </figure>
-    
-    
+
+    <div class="flexslider">
+        <ul class="slides">   
+            <li><img src="images/slide_1.jpg" alt="" class="rotating-item"></li>
+            <li><img src="images/slide_2.jpg" alt="" class="rotating-item"></li>
+            <li><img src="images/slide_3.jpg" alt="" class="rotating-item"></li>
+            <li><img src="images/slide_4.jpg" alt="" class="rotating-item"></li>
+            <li><img src="images/slide_5.jpg" alt="" class="rotating-item"></li> 
+            <li><img src="images/slide_6.jpg" alt="" class="rotating-item"></li>
+        </ul>
+    </div>
+    <?php include 'footer.php'; ?>
+
+
 </main>
-
-<?php
-include ('footer.php');
-?>
-
 </body>
+
+
 </html>
--- news.php
+++ news.php
@@ -1,13 +1,96 @@
 <?php
+$file = fopen("python/images.csv", "r");
+
+if ($debug) {
+    if ($file) {
+        print '';
+    } else {
+        print '<p<File Open Failed.</p>';
+    }
+}
+
+while (!feof($file)) {
+    $images[] = fgetcsv($file);
+}
+
+$file = fopen("python/links.csv", "r");
+
+if ($debug) {
+    if ($file) {
+        print '';
+    } else {
+        print '<p<File Open Failed.</p>';
+    }
+}
+
+while (!feof($file)) {
+    $links[] = fgetcsv($file);
+}
+
+$file = fopen("python/headlines.csv", "r");
+
+if ($debug) {
+    if ($file) {
+        print '';
+    } else {
+        print '<p<File Open Failed.</p>';
+    }
+}
+
+while (!feof($file)) {
+    $headlines[] = fgetcsv($file);
+}
+
+$file = fopen("python/dates.csv", "r");
+
+if ($debug) {
+    if ($file) {
+        print '';
+    } else {
+        print '<p<File Open Failed.</p>';
+    }
+}
+
+while (!feof($file)) {
+    $dates[] = fgetcsv($file);
+}
+
+fclose($file);
+;
 include ('top.php');
 ?>
-<main>
-    <h2>News<h2>
+<main id="main">
+    <button class="openbtn" onclick="openNav()">&#9776; Menu </button>
+    <h2>Climate</h2>
+    <article>Climate change is harmful to Sea Otters and marine species. Increase in water temperature, increase in ocean acidity, and severe storms which can pollute coastal waters are all results of climate change. Extreme weather can cause damage to coral reefs, other costal systems, and coastal communities. As ocean temperatures rise, marine species migrate to cooler waters and lose their homes. Rising temperatures can directly disturb the metabolism, life cycle, and behavior of marine species. Check out New York Times articles below to learn how climate change not only affects the lives of Sea Otters and marine life.</article>
+    <fieldset class='articles'>
+        <legend>Latest</legend>
+        <?php
+        $i = 0;
+        foreach ($images as $image) {
+            print $dates[$i][0];
+            print '<br>';
+            print '<a href="' . $links[$i][0] . '">' . $headlines[$i + 1][0] . '</a>';
+            print '<figure>';
+            print '<a href="' . $links[$i][0] . '">';
+            print '<img alt="" src="' . $image[0] . '">';
+            print '</a>';
+            print '</figure>' . PHP_EOL;
+            $i++;
+        }
+        ?>
+    </fieldset>
+    <fieldset class='videos'>
+        <legend>Videos</legend>
+        <h3>Sweet Shell Smashers! | Amazing Animal Babies: Sea Otters | Earth Unplugged</h3>
+        <iframe width="420" height="345" src="https://www.youtube.com/embed/8h_ifQndFCo">
+        </iframe>
+        <h3>Warming oceans and marine species migration: poleward bound</h3>
+        <iframe width="420" height="345" src="https://www.youtube.com/embed/dUdd83_pzdE">
+        </iframe>
+    </fieldset>
+    <?php include ('footer'); ?>
 </main>
-
-<?php
-include ('footer.php');
-?>
 
 </body>
 </html>
