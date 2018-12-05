5b6c5707bf1f336959aafca5110fafe699569f32
racheliston <44651303+racheliston@users.noreply.github.com>
2018-12-04 19:03:16 -0500

    Add files via upload

---
+++ js/aboutus.php
@@ -0,0 +1,34 @@
+<?php
+include ('top.php');
+?>
+<main id="main">
+    <button class="openbtn" onclick="openNav()">&#9776; Menu </button>
+    
+    <article>
+        <h2>About The Founders</h2>
+        <figure class="shauna">
+            <img alt="" src="images/shauna.jpg" class="picsofus">
+            <figcaption><strong>Shauna Kimura</strong></figcaption>
+            </figure>
+        
+    <figure class="rachel">
+        <img alt="" src="images/rachel.png" class="picsofus">
+        <figcaption><strong>Rachel Liston</strong></figcaption>
+        
+            </figure>
+    
+    <figure class="alex">
+        <img alt="" src="images/alex.jpg" class="picsofus">
+        <figcaption><strong>Alex Stanley</strong></figcaption>
+        
+            </figure>
+    
+          </article>
+</main>
+
+<?php
+include ('footer.php');
+?>
+
+</body>
+</html>
