e9d93c30f46f68ec0fe70b85386c194b37c7d028
racheliston <44651303+racheliston@users.noreply.github.com>
2018-12-05 15:22:31 -0500

    Add files via upload

--- about.php
+++ about.php
@@ -7,9 +7,9 @@ include ('top.php');
         <img alt="" src="images/seaotters.jpg" class="center">
             </figure>
     <article>
-        <h2>About Us</h2>
+        <h2>About Our Organization</h2>
         <p>
-            Our main goal is to get sea otters off of the Endangered Species List, and you can help us complete this goal. There are three subspecies of sea otters: The Southern Sea Otter, Northern Sea Otter, and Russian Sea Otter. As of right now, two of those subspecies, the Southern Sea Otters and the Northern Sea Otters are on the Endangered Species Act as “threatened”. But with your help, hopefully one day we can remove all the species from the Endangered List for good.
+            Our main goal is to get sea otters off of the Endangered Species List, and you can help us complete this goal. There are three subspecies of sea otters: The Southern Sea Otter, Northern Sea Otter, and Russian Sea Otter. As of right now, two of those subspecies, the Southern Sea Otters and the Northern Sea Otters are on the Endangered Species Act as “threatened”. But with your help, hopefully one day we can remove all the species from the Endangered List for good. It won't be easy, but it is beyond needed and with your help, can be made possible.
         </p>
         
         <p>
@@ -19,11 +19,19 @@ With each adoption we are enabled to further educate more people about the absol
         </p>
         
         <p>
+            Above anything else, here at Save the Sea Otters we strive to get sea otters completely removed from the endangered species list for good. With each donation, we are continuously enabled to further reach this goal. Now more than ever, these sea otters are counting on us to help them because they are unable to help themselves unfortunately. With each donation, we are able to educate those about the importance of protecting these animals. We are also able to continue our efforts to protect the sea otters from the horrible threat of oil spills. We also attempt to convince those that work in the fishing industry to coexist and live in harmony with sea otters instead of viewing them as competition and taking away their lives for their own selfish needs. Many people are unaware of the numerous negative effects that the elimination of sea otters would have on the earth, and we have made it our job to fix this lack of awareness by spreading our message.
+        </p>
+        
+        <p>
             At one point in time, decades before now, there were millions of sea otters filling our seas and doing their part to keep the ecosystem healthy. As time went on, humans stopped seeing these animals as a means to keep our environment intact and instead as a means to increase their economic standing. They began being killed for their fur, and while they are protected today from such reckless killing, there are still those choose to take their lives. Not only this, but they face many other threats to their life.
 With each adoption, you are supporting the efforts to protect sea otters from the many threats they face. 
         </p>
         
-        <p><a href="adopt.php">Adopt now</a> and show your support for the lives of the sea otters.</p>
+        <p>
+            Sea otters are unable to help themselves, so that's where we must come in. By donating money towards our conservation work, you are doing your part to help sea otters and do for them what they can't do for themselves! With just a small donation you can help save the sea otters, and in doing so help save our planet! 
+        </p>
+        
+        <p><a href="adopt.php">Adopt now</a> and show your support for the lives of the sea otters. Help our planet by helping these species.</p>
     </article>
 </main>
 
